d9383f6a59f98cd1b7a27f89647d4476b2aca082
sebastianbe <seba_b72@hotmail.com>
2016-01-20 17:11:59 -0300

    modificacion de ruta de archivos

--- recursos/lib/principal.js
+++ recursos/lib/principal.js
@@ -3,15 +3,43 @@ var cuadro = {
 	init: function(){
 
 		$('#entrada').click(function(){
-			alert("Abrir Entrada");
+			window.location = "panelSitio.php?seccion=Entrada";
 		});
 		
 		$('#salida').click(function(){
-			alert("Abrir Salida");
+			window.location = "panelSitio.php?seccion=Salida";
 		});
 		
 		$('#ahorros').click(function(){
-			alert("Abrir Ahorros");
+			window.location = "panelSitio.php?seccion=Ahorros";
+		});
+		
+		$('#usuario').click(function(){
+			window.location = "panelSitio.php?seccion=Usuario";
+		});
+		
+		$('#cerrarSesion').click(function(){
+			//cerrar sesion
+		});
+		
+		$('#configuracion').click(function(){
+			window.location = "panelSitio.php?seccion=Configuracion";
+		});
+		
+		$('#ayuda').click(function(){
+			window.location = "panelSitio.php?seccion=Ayuda";
+		});
+		
+		$('#estadisticas').click(function(){
+			window.location = "panelSitio.php?seccion=Estadisticas";
+		});
+		
+		$('#cotizacion').click(function(){
+			window.location = "panelSitio.php?seccion=Cotizacion";
+		});
+		
+		$('#cerrarSesion').click(function(){
+			window.location = "panelSitio.php?seccion=CerrarSesion";
 		});
 	}
 }
